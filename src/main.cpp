6d3c3ddf961401536d1d694a86dc9da41d793b33
matpx <9219741+matpx@users.noreply.github.com>
2023-09-26 12:09:50 +0200

    add flecs

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,4 @@
-#define SOKOL_IMPL
-#define SOKOL_GLCORE33
 #include "sokol/sokol_app.h"
-#include "sokol/sokol_gfx.h"
-#include "sokol/sokol_glue.h"
 #include "sokol/sokol_log.h"
 
 #include "renderer.hpp"
