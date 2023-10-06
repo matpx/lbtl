7168400814350d99f25d15fa31e1564cd9137ac0
matpx <9219741+matpx@users.noreply.github.com>
2023-10-06 20:52:09 +0200

    simpler

--- src/world.hpp
+++ src/world.hpp
@@ -28,7 +28,7 @@ struct World : public flecs::world {
   flecs::query<comps::Transform> query_transform = query<comps::Transform>();
 
   flecs::query<comps::Transform, const comps::Transform> query_transform_transform =
-      query_builder<comps::Transform, const comps::Transform>().term_at(2).parent().cascade().build();
+      query_builder<comps::Transform, const comps::Transform>().parent().build();
 
   flecs::query<comps::Transform, comps::RigidBody> query_transform_rigidbody =
       query<comps::Transform, comps::RigidBody>();
