6060a5aa19052bbfb5dca6632f6199f21a93ea53
matpx <9219741+matpx@users.noreply.github.com>
2023-10-06 20:33:22 +0200

    observer

--- src/physics.cpp
+++ src/physics.cpp
@@ -15,29 +15,31 @@ void init() {
   settings.gravity = reactphysics3d::Vector3(0, 0, 0);
 
   world = physicsCommon.createPhysicsWorld(settings);
+
+  world::main.observer<comps::Transform, comps::RigidBody>()
+      .event(flecs::OnSet)
+      .each([](comps::Transform &transform, comps::RigidBody &rigidbody) {
+        HMM_Vec3 &comp_translation = transform.translation;
+        HMM_Quat &comp_rotation = transform.rotation;
+
+        rigidbody._rigidbody = world->createRigidBody(reactphysics3d::Transform(
+            reactphysics3d::Vector3(comp_translation.X, comp_translation.Y, comp_translation.Z),
+            reactphysics3d::Quaternion(comp_rotation.X, comp_rotation.Y, comp_rotation.Z, comp_rotation.W)));
+
+        rigidbody._rigidbody->setLinearDamping(rigidbody.linear_damping);
+        rigidbody._rigidbody->setAngularDamping(rigidbody.angular_damping);
+      });
 }
 
 void update(const float delta_time) {
   world->update(delta_time);
 
   world::main.query_transform_rigidbody.each([](comps::Transform &transform, comps::RigidBody &rigidbody) {
-    HMM_Vec3 &comp_translation = transform.translation;
-    HMM_Quat &comp_rotation = transform.rotation;
-
-    if (!rigidbody._rigidbody) { // TODO Observer
-      rigidbody._rigidbody = world->createRigidBody(reactphysics3d::Transform(
-          reactphysics3d::Vector3(comp_translation.X, comp_translation.Y, comp_translation.Z),
-          reactphysics3d::Quaternion(comp_rotation.X, comp_rotation.Y, comp_rotation.Z, comp_rotation.W)));
-
-          rigidbody._rigidbody->setLinearDamping(rigidbody.linear_damping);
-          rigidbody._rigidbody->setAngularDamping(rigidbody.angular_damping);
-    }
-
     const reactphysics3d::Vector3 &react_position = rigidbody._rigidbody->getTransform().getPosition();
     const reactphysics3d::Quaternion &react_orientation = rigidbody._rigidbody->getTransform().getOrientation();
 
-    comp_translation = HMM_V3(react_position.x, react_position.y, react_position.z);
-    comp_rotation = HMM_Q(react_orientation.x, react_orientation.y, react_orientation.z, react_orientation.w);
+    transform.translation = HMM_V3(react_position.x, react_position.y, react_position.z);
+    transform.rotation = HMM_Q(react_orientation.x, react_orientation.y, react_orientation.z, react_orientation.w);
   });
 }
 
